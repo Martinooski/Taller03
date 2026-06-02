265c597f2d6efd22714b628b0f4f203b14c9fc38
Martinoski <martin.alvarado@alumnos.ucn.cl>
2026-06-02 10:09:18 -0400

    Update Sistema.java

--- src/taller03/Sistema.java
+++ src/taller03/Sistema.java
@@ -7,6 +7,7 @@ public class Sistema {
     private ArrayList<Hechizo> listaHechizos;
     private Lector lector;
     private ArrayList<Mago> listaMagos;
+    private int limite;
     
     public Sistema() {
         lector = new Lector();
@@ -35,5 +36,34 @@ public class Sistema {
     		
     		System.out.println(h.toString());
     	}
+    }
+    
+    public void ordenamientoBurbujaMagos() {
+    	int n = listaMagos.size();
+    	for (int i = 0; i < n - 1; i++) {
+    		for (int j = 0; j < n - i - 1; j++) {
+    			if (listaMagos.get(j).calcularPuntaje() < listaMagos.get(j + 1).calcularPuntaje()) {
+    				
+    				Mago aux = listaMagos.get(j);
+    				listaMagos.set(j, listaMagos.get(j + 1));
+    				listaMagos.set(j + 1, aux);
+    				
+    			}
+    		}
+    	}
+    }
+    
+    public void mostrarTop10() {
+
+
+        if (listaMagos.size() < 10) {
+            limite = listaMagos.size();
+        } else {
+            limite = 10;
+        }
+
+        for (int i = 0; i < limite; i++) {
+            System.out.println((i + 1) + ". " + listaMagos.get(i).getNombreMago() + " -> Puntaje: " + listaMagos.get(i).calcularPuntaje());
+        }
     }
 }
