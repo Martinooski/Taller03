cef8ed49195b13416543d76ccce8535d0dadf703
Martinoski <martin.alvarado@alumnos.ucn.cl>
2026-06-01 20:19:11 -0400

    Update Sistema.java

--- src/taller03/Sistema.java
+++ src/taller03/Sistema.java
@@ -6,15 +6,33 @@ public class Sistema {
     
     private ArrayList<Hechizo> listaHechizos;
     private Lector lector;
+    private ArrayList<Mago> listaMagos;
     
     public Sistema() {
         lector = new Lector();
         listaHechizos = lector.leerHechizos(); 
+        listaMagos = lector.leerMagos(listaHechizos);
 
     }
     
     public void mostrarHechizos() {
+        for (Hechizo h : listaHechizos) {
+            System.out.println(h.toStringSimple());
+        }
+    }
+    
+    public void mostrarMagos() {
+    	for (Mago m : listaMagos) {
+    		System.out.println(m.getNombreMago());
+    		for (Hechizo h : m.getHechizos()) {
+    			System.out.println(" -" + h.getNombrehechizo());
+    		}
+    	}
+    }
+    
+    public void mostrarHechizosYPuntaje() {
     	for (Hechizo h : listaHechizos) {
+    		
     		System.out.println(h.toString());
     	}
     }
